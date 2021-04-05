cb7e4e8f9af31796cbed6d22f1d169ffde2c2a75
Daniel Arad <sisitrs2@gmail.com>
2021-04-05 10:24:34 +0300

    Added ip function.

---
+++ ip.bat
@@ -0,0 +1,3 @@
+@echo off
+Powershell.exe -executionpolicy remotesigned -File %userprofile%/git/dtools/ip.ps1
+pause
---
+++ ip.ps1
@@ -0,0 +1,1 @@
+ipconfig | Select-String -Pattern Wi-Fi -Context 0,4 | findstr IPv4
